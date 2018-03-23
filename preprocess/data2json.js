a17f5948dd8b3ba2c7de49d29d85cfa9ca5f1d10
William <willhnguyen@gmail.com>
2018-03-22 22:38:22 -0700

    More data added
    Population and climate change data added

--- preprocess/data2json.js
+++ preprocess/data2json.js
@@ -1,15 +1,12 @@
 const fs = require('fs-sync');
 const csv2json = require('csvtojson');
 
-let gdpFile = '../data/csv/gpd_data.csv';
-let co2File = '../data/csv/co2_data.csv';
-
 // Helper function
 let convertCSV2JSON = function (inputFileName, outputFileName, idIdentifier, callback) {
     if (fs.exists(inputFileName)) {
 
         var jsondata = {data: [], ids: {}};
-        
+
         csv2json().fromFile(inputFileName)
         .on('json', (jsonRowObj)=>{
             jsondata.data.push(jsonRowObj);
@@ -22,8 +19,20 @@ let convertCSV2JSON = function (inputFileName, outputFileName, idIdentifier, cal
     }
 }
 
+// Input file locations
+let gdpFile = '../data/csv/gpd_data.csv';
+let co2File = '../data/csv/co2_data.csv';
+let popFile = '../data/csv/population_data.csv';
+let climateFile = '../data/csv/climate_change_data.csv';
+
+// Output file locations
+let gdpFileOut = '../data/json/gdp_data.json';
+let co2FileOut = '../data/json/co2_data.json';
+let popFileOut = '../data/json/population_data.json';
+let climateFileOut = '../data/json/climate_change_data.json';
+
 // Convert to JSON and save
-let gdpData;
-let co2Data;
-convertCSV2JSON(gdpFile, '../data/json/gdp_data.json', "Country Code");
-convertCSV2JSON(co2File, '../data/json/co2_data.json', "Country Code");
+convertCSV2JSON(gdpFile, gdpFileOut, "Country Code");
+convertCSV2JSON(co2File, co2FileOut, "Country Code");
+convertCSV2JSON(popFile, popFileOut, "Country Code");
+convertCSV2JSON(climateFile, climateFileOut, "Country Code");
